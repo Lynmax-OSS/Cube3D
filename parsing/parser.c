4bbd628a5781d3719b12e09f7f8985ba23442ffa
Keith Teo <keteo@u90z02s03.42kl.edu.my>
2026-04-28 14:18:01 +0800

    splitted the parse file function down to below 25 lines

--- parsing/parser.c
+++ parsing/parser.c
@@ -64,44 +64,62 @@ static int	extension_check(char *map)
 // 	validate_map(scene);
 // }
 
+static void	free_line(char *line, char *trimmed)
+{
+	free(line);
+	free(trimmed);
+}
+
+static void	check_textures(char *trimmed, t_scene *scene)
+{
+	if (!ft_strncmp(trimmed, "NO", 2) || !ft_strncmp(trimmed, "SO", 2)
+		|| !ft_strncmp(trimmed, "WE", 2) || !ft_strncmp(trimmed, "EA", 2))
+		parse_texture(trimmed, scene);
+	else if (*trimmed == 'F' || *trimmed == 'C')
+		parse_color(trimmed, scene);
+	else
+		error_exit("Unknown identifier");
+}
+
+char	*check_line(char *line, t_scene *scene, int fd, int *map_checked)
+{
+	char	*trimmed;
+
+	trimmed = ft_strtrim(line, "\r\n \t");
+	printf("%s\n", trimmed);
+	if (is_empty_line(trimmed))
+	{
+		free_line(line, trimmed);
+		return (get_next_line(fd));
+	}
+	if (is_map_line(trimmed))
+	{
+		parse_map(fd, line, scene);
+		free_line(line, trimmed);
+		*map_checked = 1;
+		return (NULL);
+	}
+	check_textures(trimmed, scene);
+	free_line(line, trimmed);
+	return (get_next_line(fd));
+}
+
 void	parse_file(char *path, t_scene *scene)
 {
 	int		fd;
 	char	*line;
-	char	*trimmed;
+	int		map_checked;
 
+	map_checked = 0;
 	if (!extension_check(path))
 		error_exit("Invalid file extension");
 	fd = open(path, O_RDONLY);
 	if (fd < 0)
 		error_exit("Cannot open file");
-	while ((line = get_next_line(fd)))
+	line = get_next_line(fd);
+	while (line != NULL && !map_checked)
 	{
-		trimmed = ft_strtrim(line, "\r\n \t");
-		if (is_empty_line(trimmed))
-		{
-			free(trimmed);
-			free(line);
-			continue ;
-		}
-		if (is_map_line(trimmed))
-		{
-			parse_map(fd, line, scene);
-			free(trimmed);
-			free(line);
-			break ;
-		}
-		if (!ft_strncmp(trimmed, "NO", 2)
-			|| !ft_strncmp(trimmed, "SO", 2)
-			|| !ft_strncmp(trimmed, "WE", 2)
-			|| !ft_strncmp(trimmed, "EA", 2))
-			parse_texture(trimmed, scene);
-		else if (*trimmed == 'F' || *trimmed == 'C')
-			parse_color(trimmed, scene);
-		else
-			error_exit("Unknown identifier");
-		free(line);
-		free(trimmed);
+		line = check_line(line, scene, fd, &map_checked);
 	}
 	close(fd);
 	validate_scene(scene);
